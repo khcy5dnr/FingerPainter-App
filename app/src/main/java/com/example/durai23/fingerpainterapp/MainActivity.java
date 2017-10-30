60030fb9f78f6ad65c3be36d77b698ea4532e06f
khcy5dnr <khcy5dnr@nottingham.edu.my>
2017-10-30 19:36:19 +0800

    Added FingerPainterView class

--- app/src/main/java/com/example/durai23/fingerpainterapp/MainActivity.java
+++ app/src/main/java/com/example/durai23/fingerpainterapp/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FingerPainterView fingerPainterView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fingerPainterView = (FingerPainterView) findViewById(R.id.canvasView);
     }
 }
